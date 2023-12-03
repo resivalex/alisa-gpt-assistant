5470afc222457e9a66bebb112dff1c3e4c47c689
Ivan Reshetnikov <resivalex@gmail.com>
2023-12-03 20:25:51 +0600

    Webhook instead of console

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import os
 import dotenv
+from fastapi import FastAPI
+import uvicorn
 
 from alisa_gpt_assistant.protocols import DialogProtocol
 from alisa_gpt_assistant import GptAssistantDialogFactory
 
 dotenv.load_dotenv()
 
-
 api_key = os.environ["OPENAI_API_KEY"]
 assistant_id = os.environ["ASSISTANT_ID"]
+webhook_path = os.environ["WEBHOOK_PATH"]
+host = os.environ["HOST"]
+port = int(os.environ["PORT"])
 
 dialog: DialogProtocol = GptAssistantDialogFactory().create(api_key, assistant_id)
 
+app = FastAPI()
 
-while True:
-    message = input("You: ")
+
+@app.get(webhook_path)
+async def webhook(message: str):
     response = dialog.send(message)
-    print(f"Bot: {response}")
+    return {"response": response}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=port)
